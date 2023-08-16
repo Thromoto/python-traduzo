628303395a7e36b693c312a11a849bb387f631aa
Thiago Omoto <thi.omoto@gmail.com>
2023-08-16 14:38:10 -0300

    CONTROLLER - Tradução de Texto - POST

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template
-# from deep_translator import GoogleTranslator
+from flask import Blueprint, render_template, request
+from deep_translator import GoogleTranslator
 from models.language_model import LanguageModel
 # from models.history_model import HistoryModel
 
@@ -11,18 +11,30 @@ translate_controller = Blueprint("translate_controller", __name__)
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
     languages = LanguageModel.list_dicts()
-    text_to_translate = "O que deseja traduzir"
-    translate_from = "pt"
-    translate_to = "en"
-    translated = "Tradução"
 
+    if request.method == "POST":
+        text_to_translate = request.form.get("text-to-translate")
+        translate_from = request.form.get("translate-from") or "pt"
+        translate_to = request.form.get("translate-to") or "en"
+
+        translated = GoogleTranslator(
+            source=translate_from, target=translate_to
+        ).translate(text_to_translate)
+
+        return render_template(
+            "index.html",
+            languages=languages,
+            text_to_translate=text_to_translate,
+            translate_from=translate_from,
+            translate_to=translate_to,
+            translated=translated,
+        )
+
+    translated = "Tradução"
     return render_template(
         "index.html",
         languages=languages,
-        text_to_translate=text_to_translate,
-        translate_from=translate_from,
-        translate_to=translate_to,
-        translated=translated,
+        translated=translated
     )
 
 
