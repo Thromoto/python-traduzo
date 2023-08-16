f22a60bca84696aa2ebc43af2b173b5892eaf128
Thiago Omoto <thi.omoto@gmail.com>
2023-08-15 21:05:28 -0300

    CONTROLLER & VIEW - Endpoint Tradutor, renderizando variáveis do Backend - GET

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template
-# from deep_translator import GoogleTranslator
-# from models.language_model import LanguageModel
+from flask import Blueprint, render_template, request
+from deep_translator import GoogleTranslator
+from models.language_model import LanguageModel
 # from models.history_model import HistoryModel
 
 
@@ -10,7 +10,29 @@ translate_controller = Blueprint("translate_controller", __name__)
 # Reqs. 4 e 5
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    languages = LanguageModel.list_dicts()
+
+    if request.method == "POST":
+        text_to_translate = "O que deseja traduzir"
+        translate_from = "pt"
+        translate_to = "en"
+        translated = "Tradução"
+
+        return render_template(
+            "index.html",
+            languages=languages,
+            text_to_translate=text_to_translate,
+            translate_from=translate_from,
+            translate_to=translate_to,
+            translated=translated,
+        )
+
+    translated = "Tradução"
+    return render_template(
+        "index.html",
+        languages=languages,
+        translated=translated
+    )
 
 
 # Req. 6
