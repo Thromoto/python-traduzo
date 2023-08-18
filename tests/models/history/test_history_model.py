4ad292bf11a63755cf014f3ab730dfa38f9b21c2
Thiago Omoto <thi.omoto@gmail.com>
2023-08-18 11:39:38 -0300

    TESTE - Histórico de Traduções

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,12 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    json_list = json.loads(HistoryModel.list_as_json())
+
+    assert len(json_list) == 2
+    assert json_list[0]["text_to_translate"] == "Hello, I like videogame"
+    assert json_list[0]["translate_from"] == "en"
+    assert json_list[0]["translate_to"] == "pt"
